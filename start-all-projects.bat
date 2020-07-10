a9cca6655478b5d5ce2e0b604a2069cff669a150
Vasil Indzhev <v.indzhev@gmail.com>
2020-07-10 18:07:14 +0300

    start-all bat added. daily bump

---
+++ start-all-projects.bat
@@ -0,0 +1,7 @@
+start /d ".\ApiGateway\Portal.Gateway\" dotnet run
+start /d ".\AuthenticationService\AuthenticationService.IDP\" dotnet run
+start /d ".\PaymentService\PaymentService.API\" dotnet run
+start /d ".\PolicySearchService\PolicySearchService.API\" dotnet run
+start /d ".\PolicyService\PolicyService.API\" dotnet run
+start /d ".\PricingService\PricingService.API\" dotnet run
+start /d ".\ProductService\ProductService.API\" dotnet run
